d03ae153aba580a92c373dab0e3ba57f76b9c6e1
kelianor <you@example.com>
2026-06-14 12:52:16 +0300

    chore: routine rpc backup (feat: added winners message, fix: return Empty according to protobuf definition)

--- telegram_bot/src/triviabot/rpc.py
+++ telegram_bot/src/triviabot/rpc.py
@@ -1,8 +1,9 @@
-import telebot
 import murder_trivia_pb2_grpc
 import murder_trivia_pb2
+from google.protobuf import empty_pb2
+import telebot
+import game
 from triviabot import bot
-from game import users, game_started, players
 
 class BotRPC(murder_trivia_pb2_grpc.BotRPCServicer):
     def startRegistration(self, request, context):
@@ -11,25 +12,27 @@ class BotRPC(murder_trivia_pb2_grpc.BotRPCServicer):
         button_not_participating = telebot.types.InlineKeyboardButton(text="Не участвую", callback_data='not_participating')
         keyboard.row(button_participating, button_not_participating)
 
-        game_started = False
+        game.game_started = False
 
-        for userID in users:
-            bot.send_message(userID, "Желаете принять участие в игре?", reply_markup=keyboard) # TODO: Поменять сообщение
+        for chat_id in game.users:
+            bot.send_message(chat_id, "Желаете принять участие в игре?", reply_markup=keyboard) # TODO: Поменять сообщение
+        return empty_pb2.Empty()
             
     def receiveRegisteredPlayers(self, request, context):
-        pass
+        return game.players
 
-    def startGame():
-        game_started = True
-        for uid in players.keys():
-            playerList = "Игра началась :) В вечеринке участвуют: " # TODO: Поменять сообщение
-            for uid in players.keys():
-                layerList += "@" + bot.get_chat(uid).username + " "
+    def startGame(self):
+        game.game_started = True
+        player_list = "Игра началась :) В вечеринке участвуют: " # TODO: Поменять сообщение
+        for chat_id in game.players.players:
+            player_list += f"@{bot.get_chat(chat_id).username} "
+        for chat_id in game.players.players:       
+            bot.send_message(chat_id, player_list)
+        return empty_pb2.Empty()
 
-        bot.send_message(uid, playerList)
     def sendNewQuestion(self, request, context):
-        if game_started == False:
-            super().startGame()
+        if game.game_started == False:
+            self.startGame()
         keyboard = telebot.types.InlineKeyboardMarkup()
         Abtn = telebot.types.InlineKeyboardButton(text="A", callback_data='answerA')
         Bbtn = telebot.types.InlineKeyboardButton(text="B", callback_data='answerB')
@@ -37,25 +40,26 @@ class BotRPC(murder_trivia_pb2_grpc.BotRPCServicer):
         Dbtn = telebot.types.InlineKeyboardButton(text="D", callback_data='answerD')
         keyboard.row(Abtn, Bbtn, Cbtn, Dbtn)
 
-        for userID in users:
-            bot.send_message(userID, "Выберите один из варинатов ответа: ", reply_markup=keyboard)
+        for chat_id in game.players.players:
+            bot.send_message(chat_id, "Выберите один из варинатов ответа: ", reply_markup=keyboard)
+        return empty_pb2.Empty()
     
     def receivePlayerAnswers(self, request, context):
-        response = murder_trivia_pb2.PlayerList
-        response[:] = players
-            
+        return game.players
 
     def correctAnswerWas(self, request, context):
-        for uid in players.keys():
-            if players[uid] == request.correct_answer:
-                bot.send_message(uid, "Вы угадали :), правильный ответ был: ") # TODO: Поменять сообщение
+        for chat_id in game.players.players:
+            if game.players.players[chat_id] == request.correct_answer:
+                bot.send_message(chat_id, "Вы угадали :), правильный ответ был: ") # TODO: Поменять сообщение
             else:
-                bot.send_message(uid, "Вы не угадали :(, правильный ответ был: ") # TODO: Поменять сообщение
+                bot.send_message(chat_id, "Вы не угадали :(, правильный ответ был: ") # TODO: Поменять сообщение
+        return empty_pb2.Empty()
     
     def assignMinigame(self, request, context):
-        userID = request.chat_id
+        chat_id = request.chat_id
+        game.players.players[chat_id].minigame = request.game_type
         match(request.game_type):
-            case murder_trivia_pb2.GAME_UNSPECIFIED:
+            case murder_trivia_pb2.GAME_UNSPECIFIED:  # TODO: Сделать миниигры
                 pass
             case murder_trivia_pb2.GAME_WIRES:
                 pass
@@ -63,12 +67,24 @@ class BotRPC(murder_trivia_pb2_grpc.BotRPCServicer):
                 pass
             case murder_trivia_pb2.GAME_MINESWEEPER:
                 pass
+        return empty_pb2.Empty()
     
     def sendYoureDead(self, request, context):
         bot.send_message(request.chat_id, "Вы сдохли!") # TODO: Поменять сообщение
+        return empty_pb2.Empty()
     
     def sendYoureAlive(self, request, context):
         bot.send_message(request.chat_id, "Вы живы!") # TODO: Поменять сообщение
+        return empty_pb2.Empty()
     
     def showWinners(self, request, context):
-        pass
+        winners_message = "Победители:\n"
+        for i, chat_id in enumerate(request.players, start=1):
+            chat = bot.get_chat(chat_id)
+            if chat.username == None:
+                winners_message += f"{i}. {chat.first_name}\n"
+            else:
+                winners_message += f"{i}. @{chat.username}\n"
+        for chat_id in game.players.players:       
+            bot.send_message(chat_id, winners_message)
+        return empty_pb2.Empty()
